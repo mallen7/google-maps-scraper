2ae648743e0c3d47849c585c9f3e6e2ec3aa55f5
Allen, Scott <MAllen@tcsg.edu>
2023-11-29 17:31:41 -0500

    added chrome.options

--- src/scraper.py
+++ src/scraper.py
@@ -19,6 +19,9 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--remote-debugging-port=9222")
 
+chrome_options.add_argument("--disable-gpu")  # Disables GPU hardware acceleration
+chrome_options.add_argument("--window-size=1920,1080")  # Sets the window size
+
 driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
 
 def process_reviews(reviews):
